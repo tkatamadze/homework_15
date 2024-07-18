1107043665a5266fe8b5b20f4dc134013370b9bb
Toma Katamadze <toma.katamadze@gmail.com>
2024-07-18 12:34:08 +0400

    Add main function

--- homework_15.py
+++ homework_15.py
@@ -5,3 +5,24 @@ def sortList(list):
                 list[i], list[j] = list[j], list[i]
 
     print("Sorted List: ", list)
+
+
+def main():
+    while True:
+        qtyElements = int(input("How many elements you would like to insert (qty less than 4 is not allowed)? "))
+        if qtyElements < 4:
+            print("Qty must not be less than 4")
+        elif qtyElements % 2 != 0:
+            print("Qty must be odd")
+        else:
+            userInput = input("Enter elements of list using space between them (Ex: 1 23 21 45): ")
+
+            userList = list(map(int,userInput.split()))
+
+            if len(userList) != qtyElements:
+                print("Please insert correct qty of elements")
+
+            else:
+                sortList(userList)
+
+                break
